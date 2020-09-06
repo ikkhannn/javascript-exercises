ada783eb35d8cb9293fb117164edf37cf77a99b5
Imran Khan <imrankhannn81@gmail.com>
2020-09-07 00:13:00 +0500

    jquery way ajax call

--- asyncjs/async.js
+++ asyncjs/async.js
@@ -1,13 +1,21 @@
 window.onload = function(){
+   //vanilajs way
+   
     var http = new XMLHttpRequest();
     http.onreadystatechange = function(){
       if(http.readyState == 4 && http.status==200){
-            console.log(JSON.parse(http.response));
+           // console.log(JSON.parse(http.response));
       }  
     };
     http.open("GET","data/tweets.json",false);
     http.send();
-    console.log("test");
 
 
-}
+    //jquery way
+    $.get("data/tweets.json",function(data){
+            console.log(data)
+    });
+    console.log("test")
+
+
+};
