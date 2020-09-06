2a3916e7c12c2e32116484d72eb622a432f3e48c
Imran Khan <imrankhannn81@gmail.com>
2020-09-07 01:49:30 +0500

    callbacks

--- asyncjs/async.js
+++ asyncjs/async.js
@@ -1,21 +1,67 @@
-window.onload = function(){
-   //vanilajs way
-   
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function(){
-      if(http.readyState == 4 && http.status==200){
-           // console.log(JSON.parse(http.response));
-      }  
-    };
-    http.open("GET","data/tweets.json",false);
-    http.send();
+// window.onload = function(){
+//    //vanilajs way
+
+//     var http = new XMLHttpRequest();
+//     http.onreadystatechange = function(){
+//       if(http.readyState == 4 && http.status==200){
+//            // console.log(JSON.parse(http.response));
+//       }
+//     };
+//     http.open("GET","data/tweets.json",false);
+//     http.send();
 
 
-    //jquery way
-    $.get("data/tweets.json",function(data){
-            console.log(data)
+//     //jquery way
+//     $.get("data/tweets.json",function(data){
+//             console.log(data)
+//     });
+//     console.log("test")
+
+
+// };
+
+function handleError(jqXHR,errorStatus,error){
+    console.log(error);
+    
+}
+
+window.onload = function () {
+
+    $.ajax({
+        type: "GET",
+        url: "data/tweets.json",
+        success: cbTweets,
+        error:handleError 
     });
-    console.log("test")
+    
 
 
-};
+    function  cbTweets(data) {
+        console.log(data);
+
+        $.ajax({
+            type: "GET",
+            url: "data/friends.json",
+            success: cbFriends,
+            error:handleError 
+        });
+    }
+
+    function cbFriends(data) {
+        console.log(data);
+
+        $.ajax({
+            type: "GET",
+            url: "data/videos.json",
+            success: function (data) {
+                console.log(data);
+
+
+            },
+            error:handleError
+        });
+
+
+    }
+
+}
