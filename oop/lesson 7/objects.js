0df5720657a5e87c93be599c65208a877f08ee86
Imran Khan <imrankhannn81@gmail.com>
2020-09-09 01:35:13 +0500

    method chaining

--- oop/lesson 7/objects.js
+++ oop/lesson 7/objects.js
@@ -3,15 +3,24 @@ class User {
     constructor(email,name){
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
 
     login(){
         console.log(this.email," is logged in");
+        return this;
     }
 
     logout(){
         console.log(this.email," is logged out") ;
+        return this;
     }
+
+    updateScore(){
+         this.score = this.score +1;
+         console.log(this.email, 'score is now ',this.score);
+        return this;
+        }
 
 
 }
@@ -19,3 +28,4 @@ class User {
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mariokorp.com','Yoshi');
 
+userOne.login().updateScore().updateScore().updateScore().logout();
