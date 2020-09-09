0da873d0a77d3407cbc1b3cd15de3da9ea7205e3
Imran Khan <imrankhannn81@gmail.com>
2020-09-10 02:07:30 +0500

    prototypes

--- oop/lesson 11/objects.js
+++ oop/lesson 11/objects.js
@@ -14,12 +14,28 @@ User.prototype.logout = function(){
     this.online = false
     console.log(this.email,'has logged out');
 }
-// ...args puts into array
 
+
+
+function Admin(...args){
+    User.apply(this,args)
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function(u){
+
+users = users.filter(function(user){
+    return user.email != u.email
+});
+
+};
 
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mariokorp.com','Yoshi');
+var admin   = new Admin('admin@example.com','Admin');
 
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne,userTwo,admin];
+admin.deleteUser(users[1]);
+console.log(admin.login());
