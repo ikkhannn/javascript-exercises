8c52ea9c3ca8d6be0b3e494af5128dad53648ef7
Imran Khan <imrankhannn81@gmail.com>
2020-09-13 16:31:08 +0500

    server side rendering

--- nodejs/requests_and_responses/app.js
+++ nodejs/requests_and_responses/app.js
@@ -3,29 +3,41 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine','ejs');
+// if views are placed other than views folder , than specify the folder name like this.
+// app.set('views', 'myviews');
 
 // listen for requests
 app.listen(3000); 
 
+
+
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+
+
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index',{title:'Home',blogs});
    
    // res.send('<h1>Hello</h1>');
 });
 
 app.get('/about',(req,res)=>{
 
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about',{title:'About'});
    // res.send('<h1>About page</h1>');
 });
 
-// redirects
-
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'Create blog'});
 });
+
 
 // 404 page by middleware, write this function at the end of page
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).render('404',{title:'404'});
 });
